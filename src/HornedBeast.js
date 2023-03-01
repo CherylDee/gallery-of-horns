7dec4ac869969480a6b81ba9c1b5d6292e7c5a44
Cheryl Dee <cdmccalop@gmail.com>
2023-03-01 13:12:43 -0800

    added images to horned beasts

---
+++ src/HornedBeast.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <>
+      <h2>Gallery of Beasts</h2>
+      <h3>UniWhal</h3>
+      <img src ="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" title="UniWhal" />
+
+      <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" title= "Unicorn Head"
+      alt="Someone wearing a very silly unicorn head mask" />
+      </>
+    )
+  }
+}
+
+export default HornedBeast;

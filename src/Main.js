6cdb911967e8576ba843d8684bfda268adede399
Cheryl Dee <cdmccalop@gmail.com>
2023-03-01 15:46:05 -0800

    added horned beast images

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import HornedBeast from './HornedBeast';
+import data from './data/data.json'
 
 
 class Main extends React.Component {
   render() {
+    
     return (
       <>
+      <main>
       <title>Horned Beasts</title>
+      {data.map((beasts) => {
+        return <HornedBeast name = {beasts.name} image_url = {beasts.image_url} />
+
+      })}
+      </main>
       </>
     )
   }

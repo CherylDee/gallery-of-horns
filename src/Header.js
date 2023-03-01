0dc5bbe1ccd04242aebda7591addfcbe4315736c
Cheryl Dee <cdmccalop@gmail.com>
2023-03-01 10:52:29 -0800

    horns

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Header extends React.Component {
+  render(){
+    return (
+    <>
+      <h1>The Gallery of Horned Beasts</h1>
+    </>
+    )
+  }
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+      <title>Horned Beasts</title>
+        <img src = "../images/beautybeast.jpg" title="beauty beast" alt ="a beautiful beast"></img>
+        <section>
+        <h2>Name of Beasts</h2>
+        <img src = "../images/battlebeast.jpg" title='battle beast' alt = "a beast ready for battle"></img>
+        <p>Description....</p>
+        </section>
+      </>
+    )
+  }
+}
+
+export default Main;

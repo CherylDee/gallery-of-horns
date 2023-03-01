10a4b25f4406994c5420e507a2db919850e2e876
Cheryl Dee <cdmccalop@gmail.com>
2023-03-01 13:42:19 -0800

    made no changes

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -9,8 +9,8 @@ class HornedBeast extends React.Component {
       <h3>UniWhal</h3>
       <img src ="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" title="UniWhal" />
 
-      <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" title= "Unicorn Head"
-      alt="Someone wearing a very silly unicorn head mask" />
+      <h3>Unicorn Head</h3>
+      <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" alt="Someone wearing a very silly unicorn head mask" title= "Unicorn Head" />
       </>
     )
   }

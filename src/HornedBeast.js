9fcb02930cd8560090748ba55f9afae4e70ec3bd
Cheryl Dee <cdmccalop@gmail.com>
2023-03-01 18:23:54 -0800

    working on display

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -11,7 +11,7 @@ class HornedBeast extends React.Component {
       <h3>{this.props.title}</h3>
       <p>{this.props.description}</p>
       <img src={this.props.image_url} alt={this.props.alt} 
-      title={this.props.title}  id={this.props.id} keyword={this.props.keyword} horns={this.props.horns}/>
+      title={this.props.title}  id={this.props.id} keyword={this.props.keyword} horns={this.props.horns} description={this.props.description}/>
     </section>
       </>
     )

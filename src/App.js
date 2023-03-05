1ca6b52c356470202dd1788d66e0f90aff753304
Cheryl Dee <cdmccalop@gmail.com>
2023-03-05 10:26:34 -0800

    no conflicts

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import HornedBeast from './HornedBeast';
 import SelectedBeast from './SelectedBeast';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import data from './data/data.json'
 
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      beast: data,
+     super(props);
+     this.state =  {
       showModal: false,
       selectedBeast: '',
       title: '',
@@ -51,40 +52,6 @@ class App extends React.Component {
     this.setState ({
       showModal: false,
     })
-
-      horns: ''
-    }
-  }
-
-  handleClick = (event) =>  {
-    let selected = event.target.value;
-    let newData = data.filter(beast => {
-      if (selected === 'All') {
-        return beast;
-      } else {
-        return beast.horns === +selected
-      }
-    })
-    this.setState({
-      beast: newData
-    })
-  }
-
- 
-
-  handleOpenModal = (title, image_url, description, horns) => {
-    this.setState({
-      showModal: true,
-      title: title,
-      image_url: image_url,
-      horns: horns
-    });
-  }
-
-  handleCloseModal = () => {
-    this.setState({
-      showModal: false
-    });
   }
 
   render() {
@@ -101,7 +68,6 @@ class App extends React.Component {
           <option value="All">All</option>
         </Form.Select>
       </Form>
-
       <Main data={this.state.beast} 
       handleOpenModal={this.handleOpenModal}/>
 
@@ -115,18 +81,12 @@ class App extends React.Component {
 
       <HornedBeast />
       <Footer />
-        
-      <Main data={this.state.beast}
-        handleOpenModal={this.handleOpenModal}/>
 
-      <SelectedBeast selectedBeast={this.state.beast}
-        title={this.state.title}
-        image_url={this.state.image_url}
-        description={this.state.description}
-        showModal={this.state.showModal}
-        handleCloseModal={this.handleCloseModal}/>
-      <Footer />
-      
+      <Modal 
+        show={this.state.showModal}
+        onHide={this.handleCloseModal}>
+
+    </Modal>
     </>
     )
   }

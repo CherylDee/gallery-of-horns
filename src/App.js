350f5d144c9461b79ae5cccc4040b168d217c9bf
Cheryl Dee <cdmccalop@gmail.com>
2023-03-05 04:11:39 -0800

    added modal

--- src/App.js
+++ src/App.js
@@ -4,16 +4,62 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import HornedBeast from './HornedBeast';
+import SelectedBeast from './SelectedBeast';
+import data from './data/data.json';
+import Modal from 'react-bootstrap/Modal';
 
 
 class App extends React.Component {
+  constructor(props) {
+     super(props);
+     this.state =  {
+      showModal: false,
+      selectedBeast: '',
+      title: '',
+      image_url: '',
+      description: '',
+      horns: '',
+     }
+  }
+
+  handleOpenModal = (title, image_url, description, horns) => {
+    this.setState ({
+      showModal: true,
+      title: title,
+      image_url: image_url,
+      description: description,
+      horns: horns,
+    })
+  }
+   
+  handleCloseModal = () => {
+    this.setState ({
+      showModal: false,
+
+    })
+  }
+
   render() {
     return (
     <>
       <Header />
-      <Main />
+      <Main data={this.state.beasts} 
+      handleOpenModal={this.handleOpenModal}/>
+      <SelectedBeast selectedBeast={this.state.beasts} 
+      title = {this.state.title}
+      image_url={this.state.image_url} 
+      description = {this.state.description} 
+      horns = {this.state.horns} 
+      openModal={this.state.showModal} 
+      closeModal = {this.state.handleCloseModal} />
       <HornedBeast />
       <Footer />
+
+      <Modal 
+        show={this.state.showModal}
+        onHide={this.handleCloseModal}>
+
+    </Modal>
     </>
     )
   }
---
+++ src/SelectedBeast.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Modal from 'react-bootstrap/Modal'
+
+class SelectedBeast extends React.Component {
+
+  render() {
+    return (
+      <>
+      <Modal show = {this.props.openModal} onHide = {this.props.closeModal}>
+        <Modal.Header closeButton>{this.props.title}</Modal.Header>
+        <Modal.Body>
+          <img src = {this.props.image_url} alt={this.props.description} width='100%'></img>
+          <p>{this.props.description}</p>
+          <p># of horns: {this.props.horns}</p>
+          </Modal.Body>
+      </Modal>
+    
+      </>
+    )
+  }
+}
+export default SelectedBeast;

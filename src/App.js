42124bdc529c614d2025846196e7308e40cfe78a
Cheryl Dee <cdmccalop@gmail.com>
2023-03-05 09:19:49 -0800

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from './Footer';
 import HornedBeast from './HornedBeast';
 import SelectedBeast from './SelectedBeast';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import data from './data/data.json'
 
 
 class App extends React.Component {
@@ -18,8 +20,24 @@ class App extends React.Component {
       image_url: '',
       description: '',
       horns: '',
+      beast: data,
      }
   }
+
+  handleClick = (event) =>  {
+    let selected = event.target.value;
+    let newData = data.filter(beast => {
+      if (selected === 'All') {
+        return beast;
+      } else {
+        return beast.horns === +selected
+      }
+    })
+    this.setState({
+      beast: newData
+    })
+  }
+
 
   handleOpenModal = (title, image_url, description, horns) => {
     this.setState ({
@@ -42,15 +60,26 @@ class App extends React.Component {
     return (
     <>
       <Header />
+      <Form>
+        <Form.Select onChange = {this.handleClick}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="100">100</option>
+          <option value="All">All</option>
+        </Form.Select>
+      </Form>
       <Main data={this.state.beasts} 
       handleOpenModal={this.handleOpenModal}/>
-      <SelectedBeast selectedBeast={this.state.beasts} 
+
+      <SelectedBeast selectedBeast={this.state.beast} 
       title = {this.state.title}
       image_url={this.state.image_url} 
       description = {this.state.description} 
       horns = {this.state.horns} 
       openModal={this.state.showModal} 
       closeModal = {this.state.handleCloseModal} />
+
       <HornedBeast />
       <Footer />
 

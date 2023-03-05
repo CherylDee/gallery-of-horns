efcafc471f6f4fc6f642c2467729019674b4c0a6
Cheryl Dee <cdmccalop@gmail.com>
2023-03-04 19:24:33 -0800

    removed form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import HornedBeast from './HornedBeast';
-import Form from './Form';
+
 
 class App extends React.Component {
   render() {
